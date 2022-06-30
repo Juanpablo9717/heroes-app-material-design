11cedd312b18b2c35ae1d867c91a8bbe7710dd26
Juanpablo9717 <74035090+Juanpablo9717@users.noreply.github.com>
2022-06-30 16:02:39 -0500

    CRUD Applied by http methods (#1)

--- src/app/heroes/pipes/image.pipe.ts
+++ src/app/heroes/pipes/image.pipe.ts
@@ -2,12 +2,22 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { Hero } from '../interfaces/heroes.interface';
 
 @Pipe({
-  name: 'image'
+  name: 'image',
+  pure: true
 })
+
+// Pipes puros o inpuros
+// Cuando el pipe pure esta en false, renderiza de nuevo cada que detecta cambios en los argumentos
 export class ImagePipe implements PipeTransform {
 
   transform(hero: Hero,): string {
-    return `assets/heroes/${hero.id}.jpg`;
+    if (!hero.id && !hero.alt_img) {
+      return 'assets/no-image.png';
+    } else if (hero.alt_img) {
+      return hero.alt_img;
+    } else {
+      return `assets/heroes/${hero.id}.jpg`;
+    }
   }
 
 }
